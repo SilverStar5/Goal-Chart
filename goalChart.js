bc4b5a0ffd449a7e930ccbc3e3ca568aeb924df8
BenotPeltier <jperrot2082@gmail.com>
2021-03-18 23:08:42 +0900

    master

--- goalChart.js
+++ goalChart.js
@@ -10,48 +10,21 @@ function goalChart(config) {
         }
     });
 
-    var ELEMENT = d3.select(config.element);
+    var region = getDataBoundary(treeData);
     var date_boundary = []; // x-axis boundary
-    var offsetY = 0; // y-axis offset
-    var preOffsetY = 0;
+    date_boundary[0] = moment(region[0]).startOf('month').toDate();
+    date_boundary[1] = moment(region[1]).endOf('month').toDate();
+
+    var ELEMENT = d3.select(config.element);
     var treeHeight = treeData.length * 120;
 
-    var time_scales = [];
-    var minTickWidth = 100;
-    var maxTickWidth = 400;
-    var tick_Height = 60;
-    var header_Gap = 20; // gap between header and chart
+    var tick_Height = 40;
+    var header_Gap = 10; // gap between header and chart
     var margin = { top: 20, right: 50, bottom: 20, left: 50 };
     var total_Width = ELEMENT[0][0].offsetWidth;
     var chart_Width = d3.max([total_Width, 400]) - margin.left - margin.right;
     var total_Height = ELEMENT[0][0].offsetHeight - 10;
     var chart_Height = d3.max([total_Height, 400]) - margin.top - tick_Height - margin.bottom;
-
-    var region = getDataBoundary(treeData);
-    date_boundary[0] = moment(region[0]).startOf('month').toDate();
-    date_boundary[1] = moment(region[1]).endOf('month').toDate();
-
-    var formatDay = d3.time.format("%a %d"),
-        formatWeek = d3.time.format("%b %d"),
-        formatMonth = function(d) {
-            var milli = (d.getTime() - 10000);
-            var vanilli = new Date(milli);
-            var mon = vanilli.getMonth();
-            var yr = vanilli.getFullYear();
-
-            // return appropriate quarter for that month
-            if ((mon + 1) % 3 == 0) {
-                return yr + " Q" + parseInt(mon / 3 + 1);
-            } else {
-                return d3.time.format("%Y %b")(d);
-            }
-        },
-        formatYear = d3.time.format("%Y");
-
-    function multiFormat(date) {
-        return (d3.time.month(date) < date ? (d3.time.week(date) < date ? formatDay : formatWeek) :
-            d3.time.year(date) < date ? formatMonth : formatYear)(date);
-    }
 
     var svg = ELEMENT
         .append('div')
@@ -63,20 +36,22 @@ function goalChart(config) {
         .domain(date_boundary)
         .range([0, chart_Width])
 
-    var yScale = d3.scale.ordinal();
+    var yScale = d3.scale.linear()
+        .domain([1, treeData.length])
+        .range([0, treeHeight]);
 
     var zoom = d3.behavior.zoom()
         .on("zoom", update)
         .scaleExtent([0.1, 40])
-        .x(xScale);
-    // .y(yScale)
+        .x(xScale)
+        // .y(yScale)
 
     var xAxis = d3.svg.axis()
         .scale(xScale)
         .orient("top")
         .ticks(5)
-        .tickSize(chart_Height)
-        .tickFormat(multiFormat);
+        .tickSize(chart_Height + header_Gap)
+        .tickFormat(formatXaxis)
 
     window.addEventListener("wheel", update, { passive: true });
 
@@ -95,7 +70,6 @@ function goalChart(config) {
         let startAt = treeData[0].startAt,
             endAt = treeData[0].endAt;
 
-
         treeData.forEach((treeItem, idx) => {
             if (treeItem.startAt < startAt) startAt = treeItem.startAt;
             if (treeItem.endAt > endAt) endAt = treeItem.endAt;
@@ -104,257 +78,77 @@ function goalChart(config) {
         return [startAt, endAt];
     }
 
+    function formatXaxis(date) {
+        var formatDay = d3.time.format("%a %d"),
+            formatWeek = d3.time.format("%b %d"),
+            formatMonth = function(d) {
+                let milli = (d.getTime() - 10000);
+                let vanilli = new Date(milli);
+                let mon = vanilli.getMonth();
+                let yr = vanilli.getFullYear();
+
+                // return appropriate quarter for that month
+                if ((mon + 1) % 3 == 0) {
+                    return yr + " Q" + parseInt(mon / 3 + 1);
+                } else {
+                    return d3.time.format("%Y %b")(d);
+                }
+            },
+            formatYear = d3.time.format("%Y");
+
+        return (d3.time.month(date) < date ? (d3.time.week(date) < date ? formatDay : formatWeek) :
+            d3.time.year(date) < date ? formatMonth : formatYear)(date);
+    }
+
     function draw() {
         let g = svg.append('g')
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
+        let h = chart_Height + tick_Height + header_Gap;
         g.append("rect")
             .attr("width", chart_Width)
-            .attr("height", chart_Height + tick_Height + header_Gap)
+            .attr("height", h)
             .attr('class', "chart-Block")
 
-        g.append("rect")
-            .attr("class", "zoom box")
+        g.append("g")
+            .attr("id", "clipTick")
+            .append("rect")
             .attr("width", chart_Width)
-            .attr("height", chart_Height + tick_Height + header_Gap)
-            .style("visibility", "hidden")
-            .attr("pointer-events", "all")
-            .call(zoom)
+            .attr("height", tick_Height + header_Gap)
+            .attr('class', "tick-Block")
 
         g.append("g")
-            .attr("transform", "translate(0," + chart_Height + ")")
+            .attr("transform", "translate(0," + h + ")")
             .attr("class", "axis")
             .call(xAxis)
-            // .call(zoom)
-
-        // defs.append("clipPath")
-        //     .attr("id", "clipTick")
-        //     .append("rect")
-        //     .attr("width", chart_Width)
-        //     .attr("height", chart_Height + tick_Height + header_Gap)
-
-        // g.append("g")
-        //     .attr("class", "tick")
-        //     .attr("clip-path", "url(#clipTick)");
+            .selectAll("text")
+            .style("text-anchor", "start")
 
         g.append("clipPath")
             .attr("id", "clipChart")
             .append("rect")
-            .attr("y", tick_Height + header_Gap)
+            .attr("y", tick_Height + header_Gap * 2)
             .attr("width", chart_Width)
-            .attr("height", chart_Height - header_Gap);
+            .attr("height", chart_Height - header_Gap * 2);
 
         g.append("g")
             .attr("class", "chart")
             .attr("clip-path", "url(#clipChart)");
 
-        svg.on("click", function() {
-            preOffsetX = 0;
-            preOffsetY = offsetY;
-        });
-
+        g.append("rect")
+            .attr("class", "zoom box")
+            .attr("width", chart_Width)
+            .attr("height", h)
+            .style("visibility", "hidden")
+            .attr("pointer-events", "all")
+            .call(zoom)
 
         update();
     }
 
     function update() {
-        yScale.domain(treeData.map(function(d, i) {
-                return i + 1;
-            }))
-            .rangeRoundBands([offsetY, offsetY + treeHeight], 0.1);
-
-
         if (zoom_update()) {
-            // setScale();
             drawTree();
-        }
-
-        function startsBefore(node) {
-            return moment(node.startAt, "MM/DD/YYYY").isBefore(date_boundary[0])
-        }
-
-        function endsAfter(node) {
-            return moment(node.endAt, "MM/DD/YYYY").isAfter(date_boundary[1]);
-        }
-
-        function getWidth(node) {
-            let nodeWidth = 0;
-            if (endsAfter(node)) {
-                nodeWidth = Math.abs(xScale(new Date(date_boundary[1])) - xScale(new Date(node.startAt)));
-            } else if (startsBefore(node)) {
-                nodeWidth = Math.abs(xScale(new Date(date_boundary[0])) - xScale(new Date(node.endAt)));
-            } else {
-                nodeWidth = getActualWidth(node);
-            }
-            return nodeWidth;
-        }
-
-        function getActualWidth(node) {
-            return Math.abs(xScale(new Date(node.endAt)) - xScale(new Date(node.startAt)));
-        }
-
-        function setScale() {
-
-            let left = moment(date_boundary[0]);
-            let right = moment(date_boundary[1]);
-            let region = moment.duration(right.diff(left)).asDays();
-            let unit = decideTickUnit(region);
-            switch (unit) {
-                case 'd':
-                    time_scales = getDaysRange(date_boundary);
-                    break;
-                case 'w':
-                    time_scales = getWeeksRange(date_boundary);
-                    break;
-                case 'm':
-                    time_scales = getMonthsRange(date_boundary);
-                    break;
-                case 'q':
-                    time_scales = getQuardsRange(date_boundary);
-                    break;
-                case 'y':
-                    time_scales = getYearsRange(date_boundary);
-                    break;
-                default:
-                    break;
-            }
-
-            function decideTickUnit(days) {
-                let tickWidth = chart_Width / days;
-
-                let w = tickWidth;
-                if (w >= maxTickWidth) return 'o';
-                if (w >= minTickWidth) return 'd';
-
-                w = tickWidth * 7
-                if (w >= minTickWidth && w < maxTickWidth) return 'w';
-
-                w = tickWidth * 30
-                if (w >= minTickWidth && w < maxTickWidth) return 'm';
-
-                w = tickWidth * 90
-                if (w >= minTickWidth && w < maxTickWidth) return 'q';
-
-                w = tickWidth * 365
-                if (w >= minTickWidth && w < maxTickWidth) return 'y';
-
-                return 'o';
-            }
-
-            function getDaysRange(boundary) {
-                var first = moment(boundary[0]);
-                let count = moment(boundary[1]).diff(first, 'days');
-
-                ranges = [];
-                for (let i = 0; i <= count; i++) {
-                    let node = {
-                        name: first.format('YYYY MMM DD'),
-                        startAt: first.toDate(),
-                    };
-
-                    first.add(1, 'd');
-
-                    node['endAt'] = first.toDate();
-                    ranges.push(node);
-                }
-                return ranges;
-            }
-
-            function getWeeksRange(boundary) {
-                var first = moment(boundary[0]);
-                let count = moment(boundary[1]).diff(first, 'weeks');
-
-                ranges = [{
-                    name: first.format('YYYY [W]WW'),
-                    startAt: first.toDate(),
-                    endAt: first.endOf('week').add(1, 'days').toDate(),
-                }];
-
-                for (let i = 0; i <= count; i++) {
-                    let node = {
-                        name: first.format('YYYY [W]WW'),
-                        startAt: first.toDate(),
-                    };
-
-                    first.add(1, 'week');
-
-                    node['endAt'] = first.toDate();
-                    ranges.push(node);
-                }
-                return ranges;
-            }
-
-            function getMonthsRange(boundary) {
-                var first = moment(boundary[0]);
-                let count = moment(boundary[1]).diff(first, 'months');
-
-                ranges = [{
-                    name: first.format('YYYY MMM'),
-                    startAt: first.toDate(),
-                    endAt: first.endOf('month').add(1, 'days').toDate(),
-                }];
-
-                for (let i = 0; i <= count; i++) {
-                    let node = {
-                        name: first.format('YYYY MMM'),
-                        startAt: first.toDate(),
-                    };
-
-                    first.add(1, 'month');
-
-                    node['endAt'] = first.toDate();
-                    ranges.push(node);
-                }
-                return ranges;
-            }
-
-            function getQuardsRange(boundary) {
-                var first = moment(boundary[0]);
-                let count = moment(boundary[1]).diff(first, 'quarters');
-
-                ranges = [{
-                    name: first.format('YYYY [Q]Q'),
-                    startAt: first.toDate(),
-                    endAt: first.endOf('quarter').add(1, 'days').toDate(),
-                }];
-
-                for (let i = 0; i <= count; i++) {
-                    let node = {
-                        name: first.format('YYYY [Q]Q'),
-                        startAt: first.toDate(),
-                    };
-
-                    first.add(1, 'quarter');
-
-                    node['endAt'] = first.toDate();
-                    ranges.push(node);
-                }
-                return ranges;
-            }
-
-            function getYearsRange(boundary) {
-                var first = moment(boundary[0]);
-                let count = moment(boundary[1]).diff(first, 'years');
-
-                ranges = [{
-                    name: first.format('YYYY'),
-                    startAt: first.toDate(),
-                    endAt: first.endOf('year').add(1, 'days').toDate(),
-                }];
-
-                for (let i = 0; i <= count; i++) {
-                    let node = {
-                        name: first.format('YYYY'),
-                        startAt: first.toDate(),
-                    };
-
-                    first.add(1, 'year');
-
-                    node['endAt'] = first.toDate();
-                    ranges.push(node);
-                }
-                return ranges;
-            }
         }
 
         function drawTree() {
@@ -568,50 +362,45 @@ function goalChart(config) {
 
                 return y;
             }
+
+            function startsBefore(node) {
+                return moment(node.startAt, "MM/DD/YYYY").isBefore(date_boundary[0])
+            }
+
+            function endsAfter(node) {
+                return moment(node.endAt, "MM/DD/YYYY").isAfter(date_boundary[1]);
+            }
+
+            function getWidth(node) {
+                let nodeWidth = 0;
+                if (endsAfter(node)) {
+                    nodeWidth = Math.abs(xScale(new Date(date_boundary[1])) - xScale(new Date(node.startAt)));
+                } else if (startsBefore(node)) {
+                    nodeWidth = Math.abs(xScale(new Date(date_boundary[0])) - xScale(new Date(node.endAt)));
+                } else {
+                    nodeWidth = getActualWidth(node);
+                }
+                return nodeWidth;
+            }
+
+            function getActualWidth(node) {
+                return Math.abs(xScale(new Date(node.endAt)) - xScale(new Date(node.startAt)));
+            }
         }
 
         function zoom_update() {
             let e = d3.event;
-            // console.log(e)
             if (e != null) {
-                //     let left = moment(date_boundary[0]);
-                //     let right = moment(date_boundary[1]);
-                //     let region = moment.duration(right.diff(left - 1)).asDays();
+                // panning y-axia
+                if (e.sourceEvent.type === "mousemove") {
+                    if (e.translate[1] > 0) e.translate[1] = 0;
+                    if (e.translate[1] < chart_Height - treeHeight) e.translate[1] = chart_Height - treeHeight;
 
-                //     if (e.scale == 1) {
-                //         // scale x-axis
-                //         let offset = e.translate[0] - preOffsetX;
-                //         preOffsetX = e.translate[0];
-
-                //         // console.log(e.translate[0], ",", offset);
-
-                //         date_boundary[0] = moment(left.subtract(offset * region / chart_Width, 'days'));
-                //         date_boundary[1] = moment(left.add(region, 'days'));
-
-                // scale y-axia
-                // offsetY = e.translate[1] + preOffsetY;
-                // if (offsetY > 0) offsetY = 0;
-                // if (offsetY < chart_Height - treeHeight) offsetY = chart_Height - treeHeight;
-            } else {
-                //         right = moment(left.add(region / e.scale, 'days'));
-                //         region = moment.duration(right.diff(moment(date_boundary[0]) - 1)).asDays();
-
-                //         let tickWidth = chart_Width / region;
-                //         scalable = true;
-                // if (e.scale > 1 && tickWidth > maxTickWidth) scalable = false;
-                // if (e.scale < 1 && tickWidth * 365 < minTickWidth) scalable = false;
-
-                //         if (scalable == true) {
-                //             date_boundary[0] = moment(date_boundary[0]);
-                //             date_boundary[1] = moment(right);
-                //         } else {
-                //             return false;
-                //         }
-
-                //     }
+                    zoom.translate(e.translate);
+                    yScale.range([e.translate[1], e.translate[1] / e.scale + treeHeight]);
+                }
             }
 
-            // svg.select('rect.zoom.box').call(zoom);
             svg.select("g.axis").call(xAxis);
             return true;
         }
